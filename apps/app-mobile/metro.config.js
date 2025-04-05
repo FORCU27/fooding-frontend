512dd850493939028709bd32c9f756d39a0dd749
moneekim <monee1001@naver.com>
2025-04-05 18:04:28 +0900

    iOS 구동 추가 세팅

--- apps/app-mobile/metro.config.js
+++ apps/app-mobile/metro.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
@@ -6,6 +7,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'), // 모노레포 루트의 node_modules
+  ],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'), // app-mobile 내부
+      path.resolve(__dirname, '../../node_modules'), // 모노레포 루트
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
