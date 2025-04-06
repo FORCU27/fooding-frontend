5ff387e5bb7ea0d4fc2971ffb80752b0e77fab29
Jiyeon Kim <chloe@hyperreality.gg>
2025-04-06 19:25:18 +0900

    feat: APP GA 연동

--- apps/app-front/src/app/layout.tsx
+++ apps/app-front/src/app/layout.tsx
@@ -1,20 +1,45 @@
-
+import Script from 'next/script';
 import { ReactNode, Suspense } from 'react';
-import MuiProvider from '../components/provider/MuiProvider';
-import IntlProvider from '../components/provider/IntlProvider';
 
+import IntlProvider from '../components/provider/IntlProvider';
+import MuiProvider from '../components/provider/MuiProvider';
+import Analytics from '@/components/GA/Analytics';
 import './globals.css';
+import { GA_TRACKING_ID } from '@/lib/GA/gtag';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            strategy='afterInteractive'
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id='gtag-init'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+            }}
+          />
+        </>
+      )}
       <body className='font-pretendard'>
         <Suspense fallback={<div>Loading...</div>}>
-            <IntlProvider>
-                <MuiProvider>
-                    {children}
-                </MuiProvider>
-            </IntlProvider>
+          <IntlProvider>
+            <MuiProvider>
+              {children}
+              <Analytics />
+            </MuiProvider>
+          </IntlProvider>
         </Suspense>
       </body>
     </html>
