3ad008ee6587b59cb1882a4bcc29c703fef7ff40
nonon <dev.nonon@gmail.com>
2025-04-15 22:19:25 +0900

    공통 eslint, prettier 설정 변경

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,14 +1,1 @@
-export default {
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  insertPragma: false,
-  arrowParens: 'always',
-  htmlWhitespaceSensitivity: 'css',
-  endOfLine: 'lf',
-};
+export default { printWidth: 100, singleQuote: true, jsxSingleQuote: true };
