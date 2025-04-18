e92ed64623ec3f73edfcb592bd49eda1c4f9feee
신상호 <94950829+nononcrust@users.noreply.github.com>
2025-04-18 17:19:32 +0900

    chore: eslint 버그 수정 (#21)
    * eslint 버그 수정
    
    * 패키지 수정

--- packages/eslint-config/react-native.js
+++ packages/eslint-config/react-native.js
@@ -1,8 +1,19 @@
-import baseConfig from "./base.js";
+import { config as baseConfig } from "./base.js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config} */
 export default [
   ...baseConfig,
+  {
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
   {
     ignores: ["metro.config.js"],
   },
