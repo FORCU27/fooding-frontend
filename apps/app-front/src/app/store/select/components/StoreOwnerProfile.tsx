4ae62bab0322f54d47707dcb2646de49f4b8264f
moneekim <monee1001@naver.com>
2025-05-13 23:04:58 +0900

    매장 선택 > 사장님 프로필 영역 퍼블리싱

--- apps/app-front/src/app/store/select/components/StoreOwnerProfile.tsx
+++ apps/app-front/src/app/store/select/components/StoreOwnerProfile.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 
 interface StoreOwnerProfileProps {
   ownerName: string;
@@ -7,12 +7,35 @@ interface StoreOwnerProfileProps {
 
 export default function StoreOwnerProfile({ ownerName, profileImageSrc }: StoreOwnerProfileProps) {
   return (
-    <div className='flex flex-col items-center justify-center bg-neutral-900 text-white w-2/5 h-screen relative'>
-      <button className='absolute top-4 left-4 text-2xl'>←</button>
-      <p className='text-lg'>안녕하세요</p>
-      <h1 className='text-2xl font-bold'>{ownerName} 사장님</h1>
-      <div className='rounded-full overflow-hidden mt-4 w-48 h-48'>
-        <Image src={profileImageSrc} alt='사장님 프로필' width={192} height={192} />
+    <div className='relative flex flex-col items-center bg-[var(--color-gray-6)] text-white w-1/2 h-screen'>
+      <button className='absolute top-[50px] left-[80px] w-[60px] h-[60px]'>
+        {/* TODO arrow-left 아이콘 분리 필요 */}
+        <svg
+          width='60'
+          height='60'
+          viewBox='0 0 60 60'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M47.5 30H12.5M12.5 30L30 47.5M12.5 30L30 12.5'
+            stroke='white'
+            strokeWidth='5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      </button>
+
+      <div className='flex flex-col items-center pt-[160px]'>
+        <p className='subtitle-1'>안녕하세요</p>
+        <div className='flex gap-2'>
+          {/* TODO headline-3-1/2 분리 필요 */}
+          <div className='headline-3'>{ownerName}</div>
+          <div className='headline-3'>사장님</div>
+        </div>
+
+        <div className='mt-[32px] w-[400px] h-[400px] rounded-full bg-neutral-600 overflow-hidden'></div>
       </div>
     </div>
   );
