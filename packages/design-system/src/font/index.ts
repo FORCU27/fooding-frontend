6bd45fa8d2d03c8a4bc0e3b46850e4dd85805e6d
nonon <dev.nonon@gmail.com>
2025-05-18 21:28:42 +0900

    font -> design-system 패키지 이름 및 구조 변경

---
+++ packages/design-system/src/font/index.ts
@@ -0,0 +1,33 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: './assets/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './assets/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './assets/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './assets/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './assets/Pretendard-ExtraBold.subset.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-pretendard',
+});
