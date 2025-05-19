a77411ad2e11973778d7318b6d8128d19dac008d
moneekim <monee1001@naver.com>
2025-05-19 22:26:50 +0900

    디스트럭처링 없이 바로 params 넘기도록 수정

--- packages/api/src/app/store/index.ts
+++ packages/api/src/app/store/index.ts
@@ -1,26 +1,11 @@
 export * from './type';
 
 import { GetStoresResponse } from './type';
-import { ENDPOINT } from '../../example';
 import { api } from '../../shared';
 
 export const storeApi = {
-  getStores: async ({
-    searchString,
-    pageNum,
-    pageSize,
-  }: {
-    searchString: string;
-    pageNum: number;
-    pageSize: number;
-  }) => {
-    const response = await api.get(`${ENDPOINT}/app/stores`, {
-      params: {
-        searchString,
-        pageNum,
-        pageSize,
-      },
-    });
+  getStores: async (params: { searchString: string; pageNum: number; pageSize: number }) => {
+    const response = await api.get(`/app/stores`, { params });
     return GetStoresResponse.parse(response);
   },
 };
--- packages/api/src/app/user/index.ts
+++ packages/api/src/app/user/index.ts
@@ -1,11 +1,10 @@
 export * from './type';
 
 import { GetUserResponse } from './type';
-import { ENDPOINT } from '../../example';
 import { api } from '../../shared';
 
 export const userApi = {
   getUser: async () => {
-    return GetUserResponse.parse(api.get(`${ENDPOINT}/app/users`));
+    return GetUserResponse.parse(api.get(`/app/users`));
   },
 };
