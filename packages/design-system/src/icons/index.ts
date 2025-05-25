a3ec3acba3e87a18de2e14ff53cc4c797791c97e
Jiyeon Kim <cleo0718@gmail.com>
2025-05-25 20:47:21 +0900

    fix: design-system/icons 수정

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -1,5 +1,11 @@
 export type { IconProps } from './type';
+export * from './arrowIcons';
+export * from './chevronIcons';
 export * from './socialIcons';
-export { default as ArrowLeftIcon } from './ArrowLeftIcon';
+
 export { default as VisibilityIcon } from './VisibilityIcon';
 export { default as VisibilityOffIcon } from './VisibilityOffIcon';
+export { default as BellIcon } from './BellIcon';
+export { default as BookmarkIcon } from './BookmarkIcon';
+export { default as MarkpinIcon } from './MarkpinIcon';
+export { default as SearchIcon } from './SearchIcon';
