0b674d75f6f0555777bab809900a34d2528ec878
Jiyeon Kim <cleo0718@gmail.com>
2025-05-31 03:17:17 +0900

    fix: 아이콘 사이즈 수정 및 추가

--- packages/design-system/src/icons/chevronIcons/index.tsx
+++ packages/design-system/src/icons/chevronIcons/index.tsx
@@ -1,7 +1,7 @@
 import { IconProps } from '../type';
 
 export const ChevronDownIcon = ({
-  size = 20,
+  size = 24,
   color = 'currentColor',
   fill = 'none',
   ...props
@@ -9,14 +9,14 @@ export const ChevronDownIcon = ({
   <svg
     width={size}
     height={size}
-    viewBox='0 0 20 20'
+    viewBox='0 0 24 24'
     fill={fill}
     color={color}
     xmlns='http://www.w3.org/2000/svg'
     {...props}
   >
     <path
-      d='M5 7.5L10 12.5L15 7.5'
+      d='M6 9L12 15L18 9'
       stroke='currentColor'
       strokeWidth='2'
       strokeLinecap='round'
@@ -34,14 +34,64 @@ export const ChevronRightIcon = ({
   <svg
     width={size}
     height={size}
-    viewBox='0 0 20 20'
+    viewBox='0 0 24 24'
     fill={fill}
     color={color}
     xmlns='http://www.w3.org/2000/svg'
     {...props}
   >
     <path
-      d='M7.5 15L12.5 10L7.5 5'
+      d='M9 18L15 12L9 6'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export const ChevronLeftIcon = ({
+  size = 24,
+  color = 'currentColor',
+  fill = 'none',
+  ...props
+}: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox='0 0 24 24'
+    fill={fill}
+    color={color}
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M15 18L9 12L15 6'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export const ChevronUpIcon = ({
+  size = 24,
+  color = 'currentColor',
+  fill = 'none',
+  ...props
+}: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox='0 0 24 24'
+    fill={fill}
+    color={color}
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M18 15L12 9L6 15'
       stroke='currentColor'
       strokeWidth='2'
       strokeLinecap='round'
