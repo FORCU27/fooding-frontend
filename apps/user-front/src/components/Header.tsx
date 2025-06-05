c6732396467e80d4c5af8fcf2de6cf74c243e01f
nonon <nononcrust@gmail.com>
2025-06-05 12:00:57 +0900

    헤더 검색인풋 플레이스홀더 추가

--- apps/user-front/src/components/Header.tsx
+++ apps/user-front/src/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
     <div className='flex justify-center items-center pl-2 pr-3 w-full h-[60px] bg-white'>
       <Image src='/images/fooding_icon.png' width={48} height={48} alt='Fooding Icon' />
       <SearchInput.Container className='mr-3'>
-        <SearchInput />
+        <SearchInput placeholder='지금 뜨는 이탈리안 레스토랑은?' />
         <SearchInput.Button />
       </SearchInput.Container>
       <div className='flex justify-between gap-4'>
