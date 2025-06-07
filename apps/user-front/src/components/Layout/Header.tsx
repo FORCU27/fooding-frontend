3a190443e22f492178e8d0cf1f0075fa107308e0
신상호 <94950829+nononcrust@users.noreply.github.com>
2025-06-07 11:09:20 +0900

    [b2c] Input, NavButton, SearchInput 컴포넌트 추가 (#80)
    * Input, NavButton, SearchInput 컴포넌트 추가
    
    * 헤더 검색인풋 플레이스홀더 추가

--- apps/user-front/src/components/Layout/Header.tsx
+++ apps/user-front/src/components/Layout/Header.tsx
@@ -1,19 +1,16 @@
 import Image from 'next/image';
 
-import { BellIcon, BookmarkIcon, SearchIcon } from '@repo/design-system/icons';
+import { SearchInput } from '@repo/design-system/components/b2c';
+import { BellIcon, BookmarkIcon } from '@repo/design-system/icons';
 
 function Header() {
   return (
     <div className='flex justify-center items-center pl-2 pr-3 w-full h-[60px] bg-white'>
       <Image src='/images/fooding_icon.png' width={48} height={48} alt='Fooding Icon' />
-      <div className='flex justify-between items-center pl-3 pr-3 mr-3 bg-gray-1 rounded-lg w-full'>
-        <input
-          type='text'
-          className='w-full h-[45px] text-[gray-1] placeholder-[gray-1] rounded-lg border-none outline-none'
-          placeholder='지금 뜨는 이탈리안 레스토랑은?'
-        />
-        <SearchIcon color='var(--color-gray-5)' />
-      </div>
+      <SearchInput.Container className='mr-3'>
+        <SearchInput placeholder='지금 뜨는 이탈리안 레스토랑은?' />
+        <SearchInput.Button />
+      </SearchInput.Container>
       <div className='flex justify-between gap-4'>
         <BookmarkIcon />
         <BellIcon />
