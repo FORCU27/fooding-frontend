e9acffa7c3de6e7988a33ea241dfa8310a9dceb0
nonon <nononcrust@gmail.com>
2025-06-10 17:45:21 +0900

    Safe Area 제거

--- apps/user-front/src/components/Layout/Screen.tsx
+++ apps/user-front/src/components/Layout/Screen.tsx
@@ -12,10 +12,11 @@ export const Screen = ({ header, bottomTab, className, children }: ScreenProps) 
     <AppScreen>
       {header}
       <main
+        // TODO: Safe Area 영역 추가
         className={cn(
-          header && `mt-[100px]`, // 헤더 60px + Safe Area 40px
-          bottomTab && `mb-[88px]`, // 하단 Safe Area 24px + 하단탭 64px
-          `h-[calc(100dvh-188px)]`, // 헤더 + 상하단 Safe Area + 하단탭
+          header && `mt-[60px]`, // 헤더 60px + Safe Area Top 40px
+          bottomTab && `mb-[64px]`, // 하단탭 64px + 하단 Safe Area Bottom 24px
+          `h-[calc(100dvh-124px)]`, // 헤더 + 하단탭 + Safe Area Top + Safe Area Bottom
           'px-grid-margin flex flex-col overflow-y-auto scrollbar-hide',
           className,
         )}
