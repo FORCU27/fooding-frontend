3f7f073a0721e0e4f28cb2f43ac8cb5322b7eb4b
nonon <nononcrust@gmail.com>
2025-06-18 15:44:07 +0900

    Home 스켈레톤 수정

--- apps/user-front/src/tabs/home/Home.tsx
+++ apps/user-front/src/tabs/home/Home.tsx
@@ -162,8 +162,8 @@ const LoadingFallback = () => {
     <div className='px-grid-margin'>
       <Skeleton shape='text' className='mt-1' width={160} height={28} />
       <Skeleton shape='square' className='mt-3 -mx-grid-margin' height={200} />
-      <Skeleton className='mt-4' width={240} height={32} />
-      <Skeleton className='mt-3' width={320} height={36} />
+      <Skeleton shape='text' className='mt-4' width={240} height={32} />
+      <Skeleton shape='text' className='mt-3' width={320} height={36} />
       <div className='mt-4 flex gap-3 overflow-hidden -mx-grid-margin px-grid-margin'>
         {Array.from({ length: 8 }).map((_, index) => (
           <div key={index} className='flex flex-col gap-1'>
