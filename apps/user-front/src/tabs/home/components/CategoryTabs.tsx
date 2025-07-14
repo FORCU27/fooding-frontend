e43459cb8571b4cd1ee5dae971c4877c6ed78700
nonon <nononcrust@gmail.com>
2025-07-14 17:13:19 +0900

    임시 코드 제거

--- apps/user-front/src/tabs/home/components/CategoryTabs.tsx
+++ apps/user-front/src/tabs/home/components/CategoryTabs.tsx
@@ -1,4 +1,4 @@
-import { ChipFilter, ChipTabs } from '@repo/design-system/components/b2c';
+import { ChipTabs } from '@repo/design-system/components/b2c';
 
 interface CategoryTabsProps {
   categories: string[];
@@ -7,17 +7,6 @@ interface CategoryTabsProps {
 export const CategoryTabs = ({ categories }: CategoryTabsProps) => (
   <div className='flex flex-col justify-between p-grid-margin gap-4'>
     <div className='subtitle-1'>오늘은 어디에서 식사할까요?</div>
-    <ChipFilter.List scrollable>
-      <ChipFilter active>필터 1</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-      <ChipFilter>필터 2</ChipFilter>
-    </ChipFilter.List>
     <ChipTabs defaultValue={`${1}`} scrollable>
       <ChipTabs.List>
         {categories.map((category, i) => (
