132b9d5361f882a97092aedd242e7410725e4d76
wj <leewj5192@gmail.com>
2025-07-19 04:50:30 +0900

    카드 컴포넌트추가

---
+++ packages/design-system/src/components/ceo/CeoCard.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CardForm } from './CardForm';
+import { CeoCard } from './CeoCard';
+import { CeoCarnSubtitle } from './CeoCarnSubtitle';
+import { CeoInput } from './CeoInput';
+import { CeoTextArea } from './CeoTextArea';
+
+const meta = {
+  title: 'Components/ceo/CeoCard',
+  component: CeoCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: false,
+    },
+  },
+} satisfies Meta<typeof CeoCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <CardForm {...args} className='w-[800px]'>
+      <div className='headline-2'>기본 정보</div>
+      <CeoCard>
+        <CeoCarnSubtitle label='업체명' required>
+          <CeoInput id='name' />
+        </CeoCarnSubtitle>
+      </CeoCard>
+      <CeoCard>
+        <CeoCarnSubtitle label='업체명' required>
+          <CeoInput id='name' inputType='search' />
+        </CeoCarnSubtitle>
+      </CeoCard>
+      <CeoCard>
+        <CeoCarnSubtitle label='매장소개' required>
+          <CeoTextArea id='name' maxLength={1000} />
+        </CeoCarnSubtitle>
+      </CeoCard>
+      <CeoCard>
+        <CeoCarnSubtitle label='매장번호' required>
+          <CeoInput id='name' />
+        </CeoCarnSubtitle>
+      </CeoCard>
+      <CeoCard>
+        <CeoCarnSubtitle label='주소' required>
+          <div>지도</div>
+          <CeoInput id='name' inputType='search' />
+          <CeoInput id='name' />
+        </CeoCarnSubtitle>
+      </CeoCard>
+      <CeoCard>
+        <CeoCarnSubtitle label='찾아오시는길' required>
+          <CeoTextArea id='name' maxLength={1000} />
+        </CeoCarnSubtitle>
+      </CeoCard>
+    </CardForm>
+  ),
+};
---
+++ packages/design-system/src/components/ceo/CeoCarnSubtitle.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+import { CeoInput } from './CeoInput';
+
+type CeoNameInputProps = {
+  label: string;
+  placeholder?: string;
+  required?: boolean;
+  children: ReactNode;
+};
+
+const CeoCarnSubtitle = ({ label, required = false, children }: CeoNameInputProps) => {
+  return (
+    <div className='grid w-full gap-[20px]'>
+      <label htmlFor='name' className='subtitle-2'>
+        {label}
+        {required && <span className='text-[#0080F8]'> *</span>}
+      </label>
+      {children}
+    </div>
+  );
+};
+
+export { CeoCarnSubtitle };
