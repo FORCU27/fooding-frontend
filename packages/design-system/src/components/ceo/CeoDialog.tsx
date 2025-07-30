47cdd570f04aee32e5b256618138d3a524d96b39
wj <leewj5192@gmail.com>
2025-07-30 09:50:22 +0900

    [CEO]CeoDialog에서 모든 애니메이션 제거

--- packages/design-system/src/components/ceo/CeoDialog.tsx
+++ packages/design-system/src/components/ceo/CeoDialog.tsx
@@ -64,26 +64,12 @@ const CeoDialogContent = ({
 }: CeoDialogContentProps) => {
   return (
     <Dialog.Portal>
-      <Dialog.Overlay
-        className='fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 duration-300'
-        style={
-          {
-            '--tw-duration': '300ms',
-            '--tw-ease': 'cubic-bezier(0.16, 1, 0.3, 1)',
-          } as React.CSSProperties
-        }
-      />
+      <Dialog.Overlay className='fixed inset-0 z-50 bg-black/80' />
       <Dialog.Content
         className={cn(
-          'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6 shadow-lg rounded-[20px] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+          'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6 shadow-lg rounded-[20px]',
           className,
         )}
-        style={
-          {
-            '--tw-duration': '300ms',
-            '--tw-ease': 'cubic-bezier(0.16, 1, 0.3, 1)',
-          } as React.CSSProperties
-        }
       >
         {children}
         {showCloseButton && (
