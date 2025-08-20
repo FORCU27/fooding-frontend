c697aabc0da64f1f16ce68462f104cf3001cafe8
Jiyeon Kim <cleo0718@gmail.com>
2025-08-21 00:28:24 +0900

    USER 가게 길찾기 추가

--- apps/user-front/src/screens/reservation-detail/ReservationDetail.tsx
+++ apps/user-front/src/screens/reservation-detail/ReservationDetail.tsx
@@ -58,6 +58,11 @@ const ReservationDetail = ({ reservationId }: StoreDetailProps) => {
     setIsAlertAccordionOpen((prev) => !prev);
   };
 
+  const getKakaoMapDirectionUrl = (latitude: number, longitude: number, name?: string) => {
+    const encodedName = encodeURIComponent(name ?? '목적지');
+    return `https://map.kakao.com/link/to/${encodedName},${latitude},${longitude}`;
+  };
+
   return (
     <div className='flex flex-col bg-gray-1'>
       <div className='flex flex-col p-5 bg-white/80'>
@@ -142,7 +147,18 @@ const ReservationDetail = ({ reservationId }: StoreDetailProps) => {
           </div>
         </div>
 
-        <Button variant='gray' size='large'>
+        <Button
+          variant='gray'
+          size='large'
+          onClick={() => {
+            const url = getKakaoMapDirectionUrl(
+              storeInfo.latitude,
+              storeInfo.longitude,
+              storeInfo.name,
+            );
+            window.open(url, '_blank');
+          }}
+        >
           <CompassIcon />
           <span className='ml-1'>길찾기</span>
         </Button>
