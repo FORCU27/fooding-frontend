67ee4e88fb940c715f36dfa2b7feb3ef4d418074
Jiyeon Kim <cleo0718@gmail.com>
2025-08-24 19:54:30 +0900

    fix: 오타 수정

--- packages/api/src/user/store-waitings/type.ts
+++ packages/api/src/user/store-waitings/type.ts
@@ -2,16 +2,16 @@ import { z } from 'zod/v4';
 
 import { ApiResponse } from '../../shared';
 
-export const WAITING_CHENNEL_VALUES = ['IN_PERSON', 'ONLINE'] as const;
-export const waitingChennel = z.enum(WAITING_CHENNEL_VALUES);
-export type WAITING_CHENNEL = (typeof WAITING_CHENNEL_VALUES)[number];
+export const WAITING_CHANNEL_VALUES = ['IN_PERSON', 'ONLINE'] as const;
+export const waitingChannel = z.enum(WAITING_CHANNEL_VALUES);
+export type WAITING_CHANNEL = (typeof WAITING_CHANNEL_VALUES)[number];
 
 export type Waiting = z.infer<typeof Waiting>;
 export const Waiting = z.object({
   userId: z.number(),
   waitingUserId: z.number(),
   storeId: z.number(),
-  channel: waitingChennel,
+  channel: waitingChannel,
   infantChairCount: z.number(),
   infantCount: z.number(),
   adultCount: z.number(),
