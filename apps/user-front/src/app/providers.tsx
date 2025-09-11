b769ac5c11ac74627b4defbdd8007c1aa2aa013f
nonon <nononcrust@gmail.com>
2025-09-11 10:36:01 +0900

    스토리북 추가

--- apps/user-front/src/app/providers.tsx
+++ apps/user-front/src/app/providers.tsx
@@ -1,12 +1,12 @@
 'use client';
 
+import { Toaster } from '@repo/design-system/components/b2c';
 import { MSWProvider } from '@repo/msw';
 import { OverlayProvider } from 'overlay-kit';
 
 import { LoginBottomSheetProvider } from '@/components/Auth/LoginBottomSheet';
 import { AuthProvider } from '@/components/Provider/AuthProvider';
 import { ReactQueryProvider } from '@/components/Provider/ReactQueryProvider';
-import { Toaster } from '@/components/Toaster';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
