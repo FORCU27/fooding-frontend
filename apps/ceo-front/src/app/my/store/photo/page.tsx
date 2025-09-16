2752c30ee15ba37560526533f9e328f2642a4106
moneekim <monee1001@naver.com>
2025-09-17 00:04:21 +0900

    GNB 셀렉트박스로 매장 변경 시, 사진 목록 업데이트

--- apps/ceo-front/src/app/my/store/photo/page.tsx
+++ apps/ceo-front/src/app/my/store/photo/page.tsx
@@ -1,37 +1,32 @@
 'use client';
 
-import { useEffect, useState, useRef } from 'react';
+import { useRef } from 'react';
 
+import { queryKeys } from '@repo/api/configs/query-keys';
 import { CardForm, PhotoCard, Button } from '@repo/design-system/components/ceo';
+import { useQuery } from '@tanstack/react-query';
 
 import { useStoreImages, useCreateImage, useDeleteImage } from '@/hooks/store/useStoreImages';
 import { useUploadFile } from '@/hooks/useUploadFile';
 
 const PhotoPage = () => {
-  const [storeId, setStoreId] = useState<number | undefined>(undefined);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const { data: selectedStore } = useQuery({
+    queryKey: [queryKeys.ceo.store.selectedStore],
+    queryFn: async () => {
+      const res = await fetch('/api/store/select', { cache: 'no-store' });
+      if (!res.ok) return null;
+      return res.json();
+    },
+  });
+
+  const storeId = selectedStore?.data?.id;
 
   const { data: images, isFetching } = useStoreImages(storeId);
   const uploadFile = useUploadFile();
   const createImage = useCreateImage();
   const deleteImage = useDeleteImage();
-
-  // 마운트 시 선택된 storeId 가져오기
-  useEffect(() => {
-    const fetchStore = async () => {
-      const res = await fetch('/api/store/select');
-      if (!res.ok) {
-        console.error('스토어 조회 실패', res.status);
-        return;
-      }
-      const data = await res.json();
-      const id = data.data.id;
-
-      setStoreId(typeof id === 'number' && !Number.isNaN(id) ? id : undefined);
-    };
-
-    fetchStore();
-  }, []);
 
   const handleAddImage = () => {
     fileInputRef.current?.click();
@@ -61,14 +56,14 @@ const PhotoPage = () => {
   };
 
   return (
-    <CardForm className=''>
+    <CardForm>
       <div className='headline-2'>사진</div>
       <div className='self-end'>
         <Button
           variant='primary'
+          className='py-[12px] px-[20px]'
           type='button'
           onClick={handleAddImage}
-          className='py-[12px] px-[20px]'
         >
           사진추가
         </Button>
