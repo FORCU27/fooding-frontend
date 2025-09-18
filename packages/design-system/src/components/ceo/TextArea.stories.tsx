7e39d9bdcfef9947431aa680ef5c0573dc0f8d8d
nonon <nononcrust@gmail.com>
2025-09-18 12:14:48 +0900

    Textarea 스타일 수정, 스토리북 예제 추가

--- packages/design-system/src/components/ceo/TextArea.stories.tsx
+++ packages/design-system/src/components/ceo/TextArea.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TextArea } from './TextArea';
+import { useState } from 'react';
 
 const meta = {
   title: 'Components/ceo/TextArea',
@@ -18,14 +19,45 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    placeholder: '내용을 입력해주세요.',
+  render: () => {
+    return <TextArea />;
+  },
+};
+
+export const WithPlaceholder: Story = {
+  render: () => {
+    return <TextArea placeholder='내용을 입력해 주세요.' />;
+  },
+};
+
+export const MinimumHeight: Story = {
+  render: () => {
+    return <TextArea className='min-h-[200px]' placeholder='내용을 입력해 주세요.' />;
   },
 };
 
 export const WithMaxLength: Story = {
-  args: {
-    placeholder: '100자 이내로 입력해주세요.',
-    maxLength: 100,
+  render: () => {
+    return <TextArea placeholder='100자 이내로 입력해주세요.' maxLength={100} />;
+  },
+};
+
+export const DefaultValue: Story = {
+  render: () => {
+    return <TextArea defaultValue='텍스트' placeholder='내용을 입력해 주세요.' />;
+  },
+};
+
+export const Controlled: Story = {
+  render: () => {
+    const [value, setValue] = useState('');
+
+    return (
+      <TextArea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder='내용을 입력해 주세요.'
+      />
+    );
   },
 };
