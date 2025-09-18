0d039efe2cd6fad26c7c79c8c764fc580669cb6a
titaniper <flutter.kr@gmail.com>
2025-09-18 23:21:44 +0900

    chore: temp

--- apps/user-front/src/libs/webview-bridge/index.ts
+++ apps/user-front/src/libs/webview-bridge/index.ts
@@ -1,5 +1,6 @@
 import { linkBridge } from '@webview-bridge/web';
 
-import { AppBridge, AppPostMessage } from '../../../../user-mobile/lib/webview-bridge';
+// import { AppBridge, AppPostMessage } from '../../../../user-mobile/lib/webview-bridge';
+import { AppBridge, AppPostMessage } from './temp';
 
 export const bridge = linkBridge<AppBridge, AppPostMessage>();
