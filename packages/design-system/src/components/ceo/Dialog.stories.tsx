5c4834b3f897fe26263284c8e9ab2862e0616b9c
nonon <nononcrust@gmail.com>
2025-09-19 10:47:34 +0900

    Dialog 예제 추가

--- packages/design-system/src/components/ceo/Dialog.stories.tsx
+++ packages/design-system/src/components/ceo/Dialog.stories.tsx
@@ -4,6 +4,8 @@ import { Dialog } from './Dialog';
 import { Button } from './Button';
 import { useState } from 'react';
 import { Input } from './Input';
+import { TextField } from './TextField';
+import { Checkbox } from './Checkbox';
 
 const meta = {
   title: 'Components/ceo/Dialog',
@@ -144,6 +146,67 @@ export const CategoryExample: Story = {
             <Dialog.Close asChild>
               <Button variant='outlined'>삭제</Button>
             </Dialog.Close>
+            <Dialog.Close asChild>
+              <Button>저장</Button>
+            </Dialog.Close>
+          </Dialog.Footer>
+        </Dialog.Content>
+      </Dialog>
+    );
+  },
+};
+
+export const MenuExample: Story = {
+  render: () => {
+    return (
+      <Dialog open>
+        <Dialog.Trigger asChild>
+          <Button>열기</Button>
+        </Dialog.Trigger>
+        <Dialog.Content>
+          <Dialog.Header>
+            <Dialog.Title>메뉴 수정</Dialog.Title>
+          </Dialog.Header>
+          <Dialog.Body className='gap-5'>
+            <TextField
+              required
+              label={
+                <div className='flex justify-between'>
+                  <TextField.Label>메뉴명</TextField.Label>
+                  <div className='flex gap-3'>
+                    <Checkbox labelText='대표' />
+                    <Checkbox labelText='추천' />
+                    <Checkbox labelText='신규' />
+                  </div>
+                </div>
+              }
+              maxGraphemeCount={20}
+            >
+              <TextField.Input />
+            </TextField>
+            <TextField
+              required
+              label={
+                <div className='flex justify-between'>
+                  <TextField.Label>가격</TextField.Label>
+                  <Checkbox labelText='변동가격' />
+                </div>
+              }
+              maxGraphemeCount={20}
+              format='currency'
+            >
+              <TextField.Input />
+              <TextField.Suffix>원</TextField.Suffix>
+            </TextField>
+            <TextField
+              label={<TextField.Label>메뉴 설명</TextField.Label>}
+              maxGraphemeCount={20}
+              format='currency'
+            >
+              <TextField.Textarea className='min-h-20' />
+            </TextField>
+          </Dialog.Body>
+          <Dialog.Footer>
             <Dialog.Close asChild>
               <Button>저장</Button>
             </Dialog.Close>
