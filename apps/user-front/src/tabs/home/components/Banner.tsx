a4f96e0527dc8f68cebedcc18427bb5e0dee3ac0
nonon <nononcrust@gmail.com>
2025-09-19 18:03:22 +0900

    배너 기능 구현

--- apps/user-front/src/tabs/home/components/Banner.tsx
+++ apps/user-front/src/tabs/home/components/Banner.tsx
@@ -1,39 +1,17 @@
 import Image from 'next/image';
-import NextLink from 'next/link';
 
-import { userBannerApi } from '@repo/api/user';
-import { Button } from '@repo/design-system/components/b2c';
-import { ChevronRightIcon } from '@repo/design-system/icons';
-import { useQuery } from '@tanstack/react-query';
+import { useFlow } from '@stackflow/react/future';
 
 import { Carousel } from '@/components/Carousel';
+import { useGetBannerList } from '@/hooks/banner/useGetBannerList';
+import { screenId } from '@/libs/stackflow';
 
 const DEFAULT_PLACEHOLDER = '/images/home/banneritem1.png';
 
 export const Banner = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['user-banners'],
-    queryFn: () =>
-      userBannerApi.list({
-        pageNum: 1,
-        pageSize: 10,
-      }),
-    staleTime: 5_000,
-  });
+  const flow = useFlow();
 
-  const banners = data?.data.list ?? [];
-
-  if (isError) {
-    return null;
-  }
-
-  if (isLoading && banners.length === 0) {
-    return (
-      <div className='h-[200px] flex items-center justify-center bg-gray-100 rounded-[12px]'>
-        <p className='body-4 text-gray-500'>배너를 불러오는 중입니다...</p>
-      </div>
-    );
-  }
+  const { data: banners } = useGetBannerList();
 
   if (banners.length === 0) {
     return null;
@@ -44,27 +22,26 @@ export const Banner = () => {
       <Carousel.Region>
         <Carousel.List>
           {banners.map((banner, index) => (
-            <Carousel.Item key={banner.id ?? index} className='h-[220px]'>
-              <Image
-                src={banner.imageUrl || DEFAULT_PLACEHOLDER}
-                alt={banner.name}
-                fill
-                className='object-cover'
-                priority={index === 0}
-              />
-              {banner.link && (
-                <Button
-                  asChild
-                  className='absolute left-6 bottom-6'
-                  size='small'
-                  variant='primary'
-                >
-                  <NextLink href={banner.link} target='_blank' rel='noopener noreferrer'>
-                    <span className='body-5 mr-2'>바로가기</span>
-                    <ChevronRightIcon size={20} color='var(--color-white)' />
-                  </NextLink>
-                </Button>
-              )}
+            <Carousel.Item key={banner.id} className='h-[220px]'>
+              <button
+                onClick={() => {
+                  if (!banner.parameters || !banner.parameters.screenId) return;
+
+                  const screenName = screenId[banner.parameters.screenId];
+
+                  if (screenName) {
+                    flow.push(screenName, {});
+                  }
+                }}
+              >
+                <Image
+                  src={banner.imageUrl || DEFAULT_PLACEHOLDER}
+                  alt={banner.name}
+                  fill
+                  className='object-cover w-full h-ful'
+                  priority={index === 0}
+                />
+              </button>
             </Carousel.Item>
           ))}
         </Carousel.List>
