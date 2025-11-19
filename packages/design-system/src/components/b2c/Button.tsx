bcde5cb6fbb4056abf3bec97aa03c371e62dcc1f
Jiyeon Kim <cleo0718@gmail.com>
2025-11-19 22:44:04 +0900

    USER 로딩 버튼 추가

--- packages/design-system/src/components/b2c/Button.tsx
+++ packages/design-system/src/components/b2c/Button.tsx
@@ -8,7 +8,7 @@ import { cn } from '../../utils';
 
 const buttonVariants = tv({
   base: cn(
-    'inline-flex justify-center items-center font-semibold whitespace-nowrap cursor-pointer',
+    'inline-flex justify-center items-center whitespace-nowrap cursor-pointer',
     'disabled:bg-gray-1 disabled:text-gray-4 disabled:pointer-events-none',
   ),
   variants: {
@@ -16,12 +16,11 @@ const buttonVariants = tv({
       primary: 'bg-primary-pink text-white active:bg-pink-button-press',
       gray: 'bg-gray-1 text-black active:bg-[#D4D8DC]',
       outlined: 'border border-gray-3 text-black bg-white active:bg-gray-1',
-      loading: 'bg-primary-pink text-white cursor-not-allowed',
     },
     size: {
-      small: 'h-[41px] px-4 rounded-[8px] text-sm',
-      banner: 'h-[36px] px-3 rounded-[8px] text-sm',
-      large: 'w-full h-[56px] rounded-[12px]',
+      small: 'h-[41px] px-4 rounded-[8px] body-5',
+      banner: 'h-[36px] px-3 rounded-[8px] body-5',
+      large: 'h-[56px] rounded-[12px] subtitle-5 px-[139px]',
     },
   },
   defaultVariants: {
@@ -33,6 +32,7 @@ const buttonVariants = tv({
 type ButtonProps = React.ComponentPropsWithRef<'button'> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    isLoading?: boolean;
   };
 
 export const Button = ({
@@ -41,6 +41,7 @@ export const Button = ({
   variant,
   size,
   asChild = false,
+  isLoading = false,
   ...props
 }: ButtonProps) => {
   const Component = asChild ? Slot.Root : 'button';
@@ -49,13 +50,17 @@ export const Button = ({
     <Component
       className={buttonVariants({ variant, size, className })}
       type={asChild ? undefined : 'button'}
-      disabled={props.disabled}
+      disabled={isLoading || props.disabled}
       {...props}
     >
-      <>
-        {variant === 'loading' && <MobileProgressDotIcon className='w-6 h-6' />}
-        {variant !== 'loading' && children}
-      </>
+      {isLoading ? (
+        <MobileProgressDotIcon
+          className='w-auto pointer-events-none'
+          height={size === 'large' ? 56 : 41}
+        />
+      ) : (
+        children
+      )}
     </Component>
   );
 };
