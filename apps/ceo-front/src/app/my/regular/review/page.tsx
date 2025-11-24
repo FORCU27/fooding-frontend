571177156b2c5cd10e34930f587f851a53d86b86
moneekim <monee1001@naver.com>
2025-11-24 10:21:16 +0900

    주석 해제

--- apps/ceo-front/src/app/my/regular/review/page.tsx
+++ apps/ceo-front/src/app/my/regular/review/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-// import { reviewApi } from '@repo/api/ceo';
-// import { queryKeys } from '@repo/api/configs/query-keys';
+import { reviewApi } from '@repo/api/ceo';
+import { queryKeys } from '@repo/api/configs/query-keys';
 import { CardForm } from '@repo/design-system/components/ceo';
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { ReviewCard } from './components/ReviewCard';
 import ReviewReply from './components/ReviewReply';
